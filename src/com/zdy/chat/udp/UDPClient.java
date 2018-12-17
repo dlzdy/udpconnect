392c331bf74f24ffb41a7f654f2532eeae8a2b3d
dlzdy <dlzdy@163.com>
2018-12-17 23:06:42 +0800

    dd

--- src/com/zdy/chat/udp/UDPClient.java
+++ src/com/zdy/chat/udp/UDPClient.java
@@ -8,20 +8,34 @@ import java.net.InetAddress;
 public class UDPClient {
 
 	public static void main(String[] args) throws IOException {
-		/*
-		 * 向服务器端发送数据
-		 */
-		// 1.定义服务器的地址、端口号、数据
-		//InetAddress address = InetAddress.getByName("192.168.2.225");
-		InetAddress address = InetAddress.getByName("192.168.10.201");
-		int port = 8800;
-		byte[] data = "user：admin;password：123".getBytes();
-		// 2/创建数据报，包含发送的数据信息
-		DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
 		// 3.创建DatagramSocket对象
 		DatagramSocket socket = new DatagramSocket();
-		// 4.向服务器发送数据报
-		socket.send(packet);
+		new Thread() {
+			public void run() {
+				try {
+					// 1.定义服务器的地址、端口号、数据
+					InetAddress address = InetAddress.getByName("192.168.2.225");
+					int port = 8800;
+					byte[] data = "user：admin;password：123".getBytes();
+					while (true) {
+						/*
+						 * 向服务器端发送数据
+						 */
+
+						// 2/创建数据报，包含发送的数据信息
+						DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+						// 4.向服务器发送数据报
+						socket.send(packet);
+						System.out.println("mySocket is " + socket.getLocalPort());
+						Thread.sleep(10000);
+
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		}.start();
 		/*
 		 * 接收服务器端响应的数据
 		 */
@@ -36,8 +50,7 @@ public class UDPClient {
 			System.out.println("I am client，server say：" + reply);
 		}
 		// 4.关闭资源*/
-		//socket.close();
+		// socket.close();
 	}
-	
 
 }
