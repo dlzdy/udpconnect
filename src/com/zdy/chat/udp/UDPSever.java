799c0994013f94e9d64d999e46dabd4a6ef5bb86
dlzdy <dlzdy@163.com>
2018-12-17 23:16:32 +0800

    dd

--- src/com/zdy/chat/udp/UDPSever.java
+++ src/com/zdy/chat/udp/UDPSever.java
@@ -3,9 +3,12 @@ package com.zdy.chat.udp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 public class UDPSever {
 	public static void main(String[] args) throws IOException {
+		Hashtable<String, DatagramPacket> udpMap = new Hashtable<String, DatagramPacket>();
 		DatagramSocket socket = new DatagramSocket(8800);
 		System.out.println("服务器即将启动，等待客户端的连接");
 		byte[] data = new byte[1024];// 创建字节数组，指定接收的数据包的大小
@@ -13,12 +16,15 @@ public class UDPSever {
 		while (true) {
 			DatagramPacket packet = new DatagramPacket(data, data.length);
 			socket.receive(packet);
-			// 此方法在接收到数据报之前会一直阻塞
-			// 4.读取数据
-			count++;
-			System.out.println("client count is " + count);
-			UDPServerThread UDPThread = new UDPServerThread(socket, packet, data);
-			UDPThread.start();
+			if(!udpMap.containsKey(packet.getSocketAddress().toString())) {
+				udpMap.put(packet.getSocketAddress().toString(), packet);
+				// 此方法在接收到数据报之前会一直阻塞
+				// 4.读取数据
+				count++;
+				System.out.println("client count is " + count);
+				UDPServerThread UDPThread = new UDPServerThread(socket, packet, data);
+				UDPThread.start();
+			}
 		}
 	}
 }
